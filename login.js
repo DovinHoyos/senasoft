857598fceefbf3cb23bc020304025de389ca7d1a
DovinHoyos_dev <dovricarbhoy@gmail.com>
2024-06-06 11:35:14 -0500

    modifications

--- login.js
+++ login.js
@@ -1,9 +1,24 @@
 class User {
-    constructor(username, password){
+    constructor(name, lastname, email, phone, username, password){
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone = phone;
         this.username = username;
         this.password = password;
     }
-
+    setName(name){
+        this.name = name;
+    }
+    setLastname(lastname){
+        this.lastname = lastname;
+    }
+    setEmail(email){
+        this.email = email;
+    }
+    setPhone(phone){
+        this.phone = phone;
+    }
     setUsername(username){
         this.username = username;
     }
@@ -24,9 +39,18 @@ class User {
 }
 
 let users = [];
+const showRegisterForm = (numRegisters) =>{
+    for(let i = 0; i < numRegisters; i++){
+        register();
+    }
 
+}
 const register = () =>{
     let user = new User();
+    user.setName(prompt('Ingrese su nombre: '));
+    user.setLastname(prompt('Ingrese su apellido: '));
+    user.setEmail(prompt('Ingrese su email: '));
+    user.setPhone(prompt('Ingrese su telefono: '));
     user.setUsername(prompt('Ingrese su usuario: '));
     let pass = prompt('Ingrese su contraseña: ');
     let passValid = prompt('Ingrese nuevamente su contraseña: ');
@@ -45,16 +69,24 @@ const login =()=>{
     let password = prompt('Ingrese contraseña:');
 
     for(i=0;i<users.length;i++){
-        if (username !== users[i].getUsername()){
-            alert('usuario no existe')
-            login(); 
+        let currentUser = users[i]
+        if (username !== currentUser.getUsername()){
+            continue;
         }
-        if(password !== users[i].getPassword()){
-            alert('contraseña incorrecta');
-            login();
+        if(password === currentUser.getPassword()){
+            alert('bienvenido')
+            return
         }
+        alert('contraseña incorrecta');
+        login();
+
     }
-    alert('bienvenido')
+    alert('usuario no existe')
+    
 }
-register();
+let numRegisters = parseInt(prompt('Ingrese numero de registros: '));
+showRegisterForm(numRegisters);
+users.forEach(u =>{
+    alert(u.toString());
+})
 login();
